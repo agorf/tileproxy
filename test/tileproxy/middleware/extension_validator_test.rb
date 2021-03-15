dc8927b83051629aef71e39933a719722c81e387
Angelos Orfanakos <git@angelos.dev>
2021-03-15 20:10:48 +0200

    Add tests for ExtensionValidator middleware

---
+++ test/tileproxy/middleware/extension_validator_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+require 'tileproxy/middleware/extension_validator'
+
+class ExtensionValidatorTest < Minitest::Test
+  def setup
+    @app = MockApp.new # TODO: Rack::Lint
+    subject = Tileproxy::Middleware::ExtensionValidator.new(@app)
+    @req = Rack::MockRequest.new(subject)
+  end
+
+  def test_unknown_content_type
+    opts = { 'tileproxy.path' => { ext: '.foo' } }
+    res = @req.get('/openstreetmap/1/2/3.foo', opts)
+
+    assert_equal(400, res.status)
+    assert_equal('text/plain', res.headers['Content-Type'])
+    assert_equal(
+      'Unknown Content-Type for requested file extension .foo',
+      res.body
+    )
+  end
+
+  def test_non_image_content_type
+    opts = { 'tileproxy.path' => { ext: '.mp3' } }
+    res = @req.get('/openstreetmap/1/2/3.foo', opts)
+
+    assert_equal(400, res.status)
+    assert_equal('text/plain', res.headers['Content-Type'])
+    assert_equal(
+      'Non-image Content-Type audio/mpeg for requested file extension .mp3',
+      res.body
+    )
+  end
+
+  def test_image_content_type
+    opts = { 'tileproxy.path' => { ext: '.png' } }
+    res = @req.get('/openstreetmap/1/2/3.foo', opts)
+
+    assert_equal(200, res.status)
+    assert_equal('text/plain', res.headers['Content-Type'])
+    assert_equal('OK', res.body)
+    assert_equal('image/png', @app.env['tileproxy.request_content_type'])
+  end
+end
