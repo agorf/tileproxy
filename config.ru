fecf3315641b7cab87fa1ff45c4460c00018da6b
Angelos Orfanakos <git@angelos.dev>
2021-03-15 18:47:32 +0200

    Group middleware with Rack::Builder

--- config.ru
+++ config.ru
@@ -1,16 +1,3 @@
-require 'rack'
-
 require_relative 'lib/tileproxy'
-require_relative 'lib/tileproxy/middleware/path_parser'
-require_relative 'lib/tileproxy/middleware/service_validator'
-require_relative 'lib/tileproxy/middleware/extension_validator'
-require_relative 'lib/tileproxy/middleware/tile_downloader'
 
-use Rack::Static, urls: %w[/], root: Tileproxy::TILE_CACHE_PATH, cascade: true
-
-use Tileproxy::Middleware::PathParser
-use Tileproxy::Middleware::ServiceValidator
-use Tileproxy::Middleware::ExtensionValidator
-use Tileproxy::Middleware::TileDownloader
-
-run Rack::Static.new(nil, urls: %w[/], root: Tileproxy::TILE_CACHE_PATH)
+run Tileproxy::App
--- lib/tileproxy.rb
+++ lib/tileproxy.rb
@@ -1,6 +1,23 @@
+require 'rack'
 require 'yaml'
+
+require_relative 'tileproxy/middleware/path_parser'
+require_relative 'tileproxy/middleware/service_validator'
+require_relative 'tileproxy/middleware/extension_validator'
+require_relative 'tileproxy/middleware/tile_downloader'
 
 module Tileproxy
   SERVICES = YAML.safe_load(open('services.yml')).freeze
   TILE_CACHE_PATH = File.join(ENV.fetch('HOME'), '.cache', 'tileproxy')
+
+  App = Rack::Builder.new do
+    use Rack::Static, urls: ['/'], root: TILE_CACHE_PATH, cascade: true
+
+    use Tileproxy::Middleware::PathParser
+    use Tileproxy::Middleware::ServiceValidator
+    use Tileproxy::Middleware::ExtensionValidator
+    use Tileproxy::Middleware::TileDownloader
+
+    run Rack::Static.new(nil, urls: ['/'], root: TILE_CACHE_PATH)
+  end
 end
